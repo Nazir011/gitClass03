fd7e5f21ce4d0b4686e567e4a7f1a9fc7ba8f5c3
Nazir011 <nazeer.naimi05@gmail.com>
2023-08-02 19:22:46 -0400

    the code that tells me the avialabilty of the product

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        boolean productAvailable= true;
+
+        if(true) {
+            System.out.println("yes");
+        }else {
+            System.out.println("no");
+        }
+
+        }
+    }
